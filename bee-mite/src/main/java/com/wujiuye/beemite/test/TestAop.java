c5051afbab1f47fb66398725449b536be70f43d1
wujiuye <jiuye.wu@ichestnuts.com>
2020-03-28 18:34:33 +0800

    添加license注释

--- bee-mite/src/main/java/com/wujiuye/beemite/test/TestAop.java
+++ bee-mite/src/main/java/com/wujiuye/beemite/test/TestAop.java
@@ -1,7 +1,19 @@
+/**
+ * Copyright [2019-2020] [wujiuye]
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.wujiuye.beemite.test;
-
-import com.wujiuye.beemite.asmip.AopManager;
-import com.wujiuye.beemite.ipevent.InsertPileManager;
 
 import java.io.IOException;
 import java.util.Map;
@@ -9,9 +21,9 @@ import java.util.Map;
 /**
  * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  * <p>
- * 微信公众号id：code_skill
+ * 微信公众号：Java艺术
  * QQ邮箱：419611821@qq.com
- * 微信号：www_wujiuye_com
+ * 微信号：ye_shao_ismy
  * <p>
  * ======================^^^^^^^==============^^^^^^^============
  *
